900952808788dd416cab43d4ddf3e47d0b183229
bashe <bashe@LOGANSRUN>
2025-10-13 22:47:56 -0700

    Add project files.

---
+++ Mods/Speedboost.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StupidTemplate.Mods
+{
+    internal class Speedboost
+    {
+        public static void SpeedBoost()
+        {
+            GorillaLocomotion.GTPlayer.Instance.maxJumpSpeed = 7.8f;
+            GorillaLocomotion.GTPlayer.Instance.jumpMultiplier = 3.9f;
+        }
+
+        public static void speedboostRG()
+        {
+            if (ControllerInputPoller.instance.rightGrab)
+            {
+                SpeedBoost();
+            }
+        }
+
+        public static void compspeedboost()
+        {
+            GorillaLocomotion.GTPlayer.Instance.maxJumpSpeed = 6.8f;
+            GorillaLocomotion.GTPlayer.Instance.jumpMultiplier = 3.5f;
+        }// simple speedboost mods for beginners, if you have any questions on how to start coding my dms are open
+    }
+}
---
+++ PluginInfo.cs
@@ -0,0 +1,10 @@
+﻿namespace StupidTemplate
+{
+    public class PluginInfo
+    {
+        public const string GUID = "org.Guicy.gorillatag.menutemplate";
+        public const string Name = "Guicy's First Menu!";
+        public const string Description = "Created by @Guicy with LOTS of tut <3 also ty iidk for menu template!";
+        public const string Version = "1.0.0";
+    }
+}
